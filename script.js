e905a121d535515dfe181ef13d3608df15c86021
Mark Mooney <mjmoon15@gmail.com>
2020-06-09 21:04:08 -0400

    built basic page framework on index.html and started creating variables on script.js

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
-console.log("Hello")
+var startButton = document.getElementById("#startButton");
+var answerOne = document.getElementById("#answer1");
+var answerTwo = document.getElementById("#answer2");
+var answerThree = document.getElementById("#answer3");
+var answerFour = document.getElementById("#answer4");
+var viewHighscores = document.getElementById("#highscores");
+
+var questionOne = {
+    1: "strings",
+    2: "booleans",
+    3: "alerts",
+    4: "numbers"
+}
+var questionTwo = {
+    1: "quotes",
+    2: "curly brackets",
+    3: "parentheses",
+    4: "square brackets"
+}
+var questionThree = {
+    1: "numbers and strings",
+    2: "other arrays",
+    3: "booleans",
+    4: "all of the above"
+}
+var questionFour = {
+    1: "commas",
+    2: "curly brackets",
+    3: "quotes",
+    4: "parentheses"
+}
+var questionFive = {
+    1: "JavaScript",
+    2: "terminal/bash",
+    3: "for loops",
+    4: "console.log"
+}
+
+var highScores = []
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+
+
+
+function startButton(){
+}
+// THEN a timer starts and I am presented with a question
+function startTimer(){
+}
+// WHEN I answer a question
+function askQuestions(){
+}
+// THEN I am presented with another question
+function nextQuestion(){
+}
+// WHEN I answer a question incorrectly
+function incorrect(){
+}
+// THEN time is subtracted from the clock
+function subtractTime(){
+}
+// WHEN all questions are answered or the timer reaches 0
+function allDone(){
+}
+// THEN the game is over
+function gameOver(){
+}
+// WHEN the game is over
+// THEN I can save my initials and score
+function leaderBoard(){   
+}
